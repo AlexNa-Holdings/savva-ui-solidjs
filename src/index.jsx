2e17b50ed50b736dd77ae975e8ff5fa016cba970
Alex Na <alexn@lexansoft.com>
2025-09-09 12:54:38 -0700

    release: v0.135

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,20 @@
 // src/index.jsx
+
+// --- START DEBUG TRACE FOR LOCALSTORAGE ---
+if (typeof window !== "undefined" && window.localStorage) {
+  const originalSetItem = window.localStorage.setItem;
+  window.localStorage.setItem = function(key, value) {
+    if (key === 'lang') {
+      console.groupCollapsed(`[storage-trace] localStorage.setItem('lang', '${value}')`);
+      console.trace("Stack trace:");
+      console.groupEnd();
+    }
+    originalSetItem.apply(this, arguments);
+  };
+}
+// --- END DEBUG TRACE ---
+
+
 import { render } from "solid-js/web";
 import "./index.css";
 import App from "./x/App.jsx";
